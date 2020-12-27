6904538bebe1e3c0b95f0d320fa18e07b1eb656a
Alex Vanderbist <alex.vanderbist@gmail.com>
2020-12-27 11:11:24 +0100

    wip

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::any('test', function (): void {
+    $start = microtime(true);
+    dispatch(new ProcessRawReportJob()); // temp
+    logger("Dispatch time: ".(microtime(true) - $start));
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
